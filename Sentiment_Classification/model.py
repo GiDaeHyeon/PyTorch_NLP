b4a5380b166dddceff4c23c6fd098c50249086e7
GiDaeHyeon <spliter2157@gmail.com>
2021-09-21 20:07:32 +0900

    Sentiment Classification Done!

--- Sentiment_Classification/model.py
+++ Sentiment_Classification/model.py
@@ -1,1 +1,76 @@
-from torch.nn import Module, Embedding, LSTM, Linear, Dropout, LeakyReLU, Sequentialfrom torch.nn.functional import log_softmaxfrom transformers import BertModelclass BertModelForSentimentClassification(Module):    def __init__(self,                 weight=None,                 n_classes=None,                 freeze=True):        super().__init__()        self.model = BertModel.from_pretrained(weight)        if freeze:            for param in self.model.parameters():                param.require_grad = False        self.clf = Sequential(            Linear(768, 768),            Dropout(p=.3),            LeakyReLU(),            Linear(768, 256),            Dropout(p=.3),            LeakyReLU(),            Linear(256, n_classes)        )    def forward(self, input_ids, attention_mask, token_type_ids):        x = self.model(input_ids=input_ids,                       attention_mask=attention_mask,                       token_type_ids=token_type_ids)        x = self.clf(x.pooler_output)        return log_softmax(x)class BaseLineModelForSentimentClassification(Module):    def __init__(self,                 vocab_size=None,                 wordvec_dim=None,                 hidden_size=None,                 num_layers=4,                 dropout_p=.3,                 bidirectional=True):        self.Embedding = Embedding(vocab_size, wordvec_dim)        self.lstm = LSTM(input_size=wordvec_dim,                         hidden_size=hidden_size,                         num_layers=num_layers,                         dropout=dropout_p,                         batch_first=True,                         bidirectional=bidirectional)        if bidirectional:            hidden_size *= 2        self.clf = Sequential(            Linear(768, 768),            Dropout(p=.3),            LeakyReLU(),            Linear(768, 256),            Dropout(p=.3),            LeakyReLU(),            Linear(256, 2)        )    def forward(self, x):        wordvec = self.Embedding(x)        logit, _ = self.lstm(wordvec)        y_hat = log_softmax(logit[:, -1])        return y_hat
+from torch.nn import Module, Embedding, LSTM, Linear, Dropout, LeakyReLU, Sequential, LogSoftmax
+from transformers import AutoModel
+from common import weight_he_init, weight_xavier_init
+
+
+class BertModelForSentimentClassification(Module):
+    def __init__(self,
+                 weight=None,
+                 n_classes=None,
+                 freeze=True):
+        super().__init__()
+        self.model = AutoModel.from_pretrained(weight)
+
+        if freeze:
+            for param in self.model.parameters():
+                param.require_grad = False
+
+        self.clf = Sequential(
+            Linear(768, 768),
+            Dropout(p=.3),
+            LeakyReLU(),
+            Linear(768, 256),
+            Dropout(p=.3),
+            LeakyReLU(),
+            Linear(256, n_classes)
+        )
+        self.activation = LogSoftmax()
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        x = self.model(input_ids=input_ids,
+                       attention_mask=attention_mask,
+                       token_type_ids=token_type_ids)
+        x = self.clf(x.pooler_output)
+        y_hat = self.activation(x)
+        return y_hat
+
+
+class BaseLineModelForSentimentClassification(Module):
+    def __init__(self,
+                 vocab_size=None,
+                 wordvec_dim=None,
+                 hidden_size=None,
+                 num_layers=4,
+                 dropout_p=.3,
+                 bidirectional=True):
+        super().__init__()
+        self.embedding = Embedding(vocab_size, wordvec_dim)
+        self.lstm = LSTM(input_size=wordvec_dim,
+                         hidden_size=hidden_size,
+                         num_layers=num_layers,
+                         dropout=dropout_p,
+                         batch_first=True,
+                         bidirectional=bidirectional)
+        self.activation = LogSoftmax(dim=-1)
+
+        if bidirectional:
+            hidden_size *= 2
+
+        self.clf = Sequential(
+            Linear(hidden_size, 768),
+            Dropout(p=.3),
+            LeakyReLU(),
+            Linear(768, 256),
+            Dropout(p=.3),
+            LeakyReLU(),
+            Linear(256, 2)
+        )
+        # weight_he_init(self.clf)
+        # weight_xavier_init(self.clf)
+
+    def forward(self, x):
+        wordvec = self.embedding(x)
+        hidden_state, _ = self.lstm(wordvec)
+        logit = self.clf(hidden_state)
+        y_hat = self.activation(logit[:, -1])
+        return y_hat
--- Sentiment_Classification/train.py
+++ Sentiment_Classification/train.py
@@ -1,1 +1,182 @@
-import torch.nn as nnimport torch.optim as optimfrom pytorch_lightning import LightningModule, Trainerfrom pytorch_lightning.loggers import TensorBoardLoggerfrom pytorch_lightning.callbacks.early_stopping import EarlyStoppingfrom pytorch_lightning.callbacks import ModelCheckpointfrom pytorch_lightning.plugins import DDPPluginfrom torchmetrics import Accuracy, F1, Recall, Precisionfrom model import *from dataloader import *import warningsimport argparsefrom ..common import str2boolwarnings.filterwarnings('ignore')parser = argparse.ArgumentParser(description='Naver Movie Review dataset Classification')parser.add_argument('--data_directory',                    help="dataset's directory",                    default='./dataset', type=str)parser.add_argument('--weight',                    help="BERT model's weight",                    default='kykim/bert-kor-base', type=str)parser.add_argument('--wordvec_dim',                    help="word vector's dim",                    default=512, type=int)parser.add_argument('--hidden_size',                    help="lstm's hidden size",                    default=1024, type=int)parser.add_argument('--freeze',                    help='freeze weight',                    default=True, type=str2bool)parser.add_argument('--n_class',                    help='num of classes',                    default=2, type=int)parser.add_argument('--learning_rate',                    help='learning rate',                    default=1e-3, type=float)parser.add_argument('--gpus',                    help='gpu num',                    default=[2, 3])parser.add_argument('--max_epochs',                    help='max epochs',                    default=100, type=int)parser.add_argument('--batch_size',                    help='batch size',                    default=128, type=int)parser.add_argument('--num_workers',                    help='num workers',                    default=4, type=int)parser.add_argument('--max_length',                    help='max length',                    default=256, type=int)parser.add_argument('--log_dir',                    help='tensorboard log directory',                    default='nsmc_log', type=str)parser.add_argument('--log_name',                    help='tensorboard log name',                    type=str)parser.add_argument('--ckpt_dir',                    help='checkpoint file directory',                    type=str)parser.add_argument('--ckpt_name',                    help='checkpoint file name',                    type=str)args = parser.parse_args()class Classifier(LightningModule):    def __init__(self,                 model_=None,                 lr=args.learning_rate,                 num_classes=args.n_class):        self.model = model_        self.loss_fn = nn.NLLLoss()        self.accuracy = Accuracy()        self.f1 = F1(num_classes=num_classes)        self.recall = Recall(num_classes=num_classes)        self.precision = Precision(num_classes=num_classes)        self.lr = lr    def forward(self, x):        return self.model(x)    def configure_optimizers(self):        optimizer = optim.AdamW(self.model.parameters(),                                lr=self.lr)        return optimizer    def training_step(self, batch, batch_idx):        text, label = batch        y_hat = self(text)        loss = self.loss_fn(y_hat, label)        self.log('train_loss', loss, on_step=True, on_epoch=False, prog_bar=True)        self.log('train_loss_epoch', loss, on_step=False, on_epoch=True, prog_bar=False)    def validation_step(self, batch, batch_idx):        text, label = batch        y_hat = self(text)        loss = self.loss_fn(y_hat, label)        self.accuracy(y_hat, label)        self.f1(y_hat, label)        self.precision(y_hat, label)        self.recall(y_hat, label)        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=False)        self.log('Accuracy', self.accuracy, on_step=False, on_epoch=True)        self.log('F1', self.f1, on_step=False, on_epoch=True)        self.log('Precision', self.f1, on_step=False, on_epoch=True)        self.log('Recall', self.recall, on_step=False, on_epoch=True)######################## pytorch lightning config#######################logger = TensorBoardLogger(    save_dir=args.log_dir,    name=args.log_name,    default_hp_metric=False)checkpoint_callback = ModelCheckpoint(    monitor='Accuracy',    dirpath=args.ckpt_dir,    filename=args.ckpt_name,    mode='max')early_stop_callback = EarlyStopping(    monitor='Accuracy',    min_delta=1e-4,    patience=5,    verbose=False,    mode='max')trainer = Trainer(max_epochs=args.max_epochs,                  logger=logger,                  gpus=args.gpus,                  accelerator='ddp',                  gradient_clip_val=3.,                  gradient_clip_algorithm='norm',                  plugins=DDPPlugin(find_unused_parameters=False),                  callbacks=[early_stop_callback,                             checkpoint_callback])if __name__ == '__main__':    # BaseLine    datamodule = BaseLineDataModule(        batch_size=args.batch_size,        max_length=args.max_length    )    classifier = Classifier(model_=BaseLineModelForSentimentClassification(        vocab_size=len(datamodule),        wordvec_dim=args.wordvec_dim,        hidden_size=args.hidden_size,    ))    # Bert    # classifier = Classifier()    # datamodule = BERTDataModule()    trainer.fit(model=classifier, datamodule=datamodule)
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.plugins import DDPPlugin
+
+from torchmetrics import Accuracy, F1, Recall, Precision
+
+from model import *
+from dataloader import *
+
+import warnings
+import argparse
+from common import str2bool
+
+warnings.filterwarnings('ignore')
+
+parser = argparse.ArgumentParser(description='Naver Movie Review dataset Classification')
+parser.add_argument('--data_directory',
+                    help="dataset's directory",
+                    default='./dataset', type=str)
+# monologg/kobert skt/kobert-base-v1
+parser.add_argument('--weight',
+                    help="BERT model's weight",
+                    default="ainize/klue-bert-base-mrc", type=str)
+parser.add_argument('--wordvec_dim',
+                    help="word vector's dim",
+                    default=512, type=int)
+parser.add_argument('--hidden_size',
+                    help="lstm's hidden size",
+                    default=1024, type=int)
+parser.add_argument('--freeze',
+                    help='freeze weight',
+                    default=True, type=str2bool)
+parser.add_argument('--n_class',
+                    help='num of classes',
+                    default=2, type=int)
+parser.add_argument('--learning_rate',
+                    help='learning rate',
+                    default=1e-4, type=float)
+parser.add_argument('--gpus',
+                    help='gpu num',
+                    default=[2])
+parser.add_argument('--max_epochs',
+                    help='max epochs',
+                    default=10, type=int)
+parser.add_argument('--batch_size',
+                    help='batch size',
+                    default=128, type=int)
+parser.add_argument('--num_workers',
+                    help='num workers',
+                    default=4, type=int)
+parser.add_argument('--max_length',
+                    help='max length',
+                    default=32, type=int)
+parser.add_argument('--log_dir',
+                    help='tensorboard log directory',
+                    default='nsmc_log', type=str)
+parser.add_argument('--log_name',
+                    help='tensorboard log name',
+                    type=str)
+parser.add_argument('--ckpt_dir',
+                    help='checkpoint file directory',
+                    default='nsmc_ckpt', type=str)
+parser.add_argument('--ckpt_name',
+                    help='checkpoint file name',
+                    type=str)
+args = parser.parse_args()
+
+
+class Classifier(LightningModule):
+    def __init__(self,
+                 model_=None,
+                 lr=args.learning_rate,
+                 num_classes=args.n_class):
+        super().__init__()
+        self.model = model_
+        self.loss_fn = nn.NLLLoss()
+
+        self.accuracy = Accuracy()
+        self.f1 = F1(num_classes=num_classes)
+        self.recall = Recall(num_classes=num_classes)
+        self.pre = Precision(num_classes=num_classes)
+        self.lr = lr
+
+    def forward(self, input_ids, attention_masks, token_type_ids):
+        return self.model(input_ids, attention_masks, token_type_ids)
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.model.parameters(),
+                                lr=self.lr)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        # text, label = batch.text, batch.label
+        input_ids, attention_masks, token_type_ids, label = batch
+        y_hat = self(input_ids, attention_masks, token_type_ids)
+        loss = self.loss_fn(y_hat, label)
+
+        self.log('train_loss', loss, on_step=True, on_epoch=False, prog_bar=False)
+        self.log('train_loss_epoch', loss, on_step=False, on_epoch=True, prog_bar=False)
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        # text, label = batch.text, batch.label
+        input_ids, attention_masks, token_type_ids, label = batch
+        y_hat = self(input_ids, attention_masks, token_type_ids)
+        loss = self.loss_fn(y_hat, label)
+
+        self.accuracy(preds=y_hat, target=label)
+        self.f1(preds=y_hat, target=label)
+        self.pre(preds=y_hat, target=label)
+        self.recall(preds=y_hat, target=label)
+
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log('Accuracy', self.accuracy, on_step=False, on_epoch=True)
+        self.log('F1', self.f1, on_step=False, on_epoch=True)
+        self.log('Precision', self.pre, on_step=False, on_epoch=True)
+        self.log('Recall', self.recall, on_step=False, on_epoch=True)
+        return {'loss': loss}
+
+
+#######################
+# pytorch lightning config
+#######################
+
+logger = TensorBoardLogger(
+    save_dir=args.log_dir,
+    name=args.log_name,
+    default_hp_metric=False
+)
+
+checkpoint_callback = ModelCheckpoint(
+    monitor='Accuracy',
+    dirpath=args.ckpt_dir,
+    filename=args.ckpt_name,
+    mode='max'
+)
+
+early_stop_callback = EarlyStopping(
+    monitor='Accuracy',
+    min_delta=1e-4,
+    patience=5,
+    verbose=False,
+    mode='max'
+)
+
+trainer = Trainer(max_epochs=args.max_epochs,
+                  logger=logger,
+                  gpus=args.gpus,
+                  accelerator='ddp',
+                  gradient_clip_val=3.,
+                  gradient_clip_algorithm='norm',
+                  plugins=DDPPlugin(find_unused_parameters=False),
+                  callbacks=[early_stop_callback,
+                             checkpoint_callback])
+
+if __name__ == '__main__':
+    # BaseLine
+    # datamodule = BaseLineDataModule(
+    #     batch_size=args.batch_size,
+    #     max_length=args.max_length
+    # )
+    # classifier = Classifier(model_=BaseLineModelForSentimentClassification(
+    #     vocab_size=len(datamodule),
+    #     wordvec_dim=args.wordvec_dim,
+    #     hidden_size=args.hidden_size,
+    # ))
+
+    # Bert
+    model = BertModelForSentimentClassification(weight=args.weight,
+                                                n_classes=args.n_class)
+    classifier = Classifier(model_=model)
+    datamodule = BERTDataModule(weight=args.weight,
+                                batch_size=args.batch_size,
+                                num_workers=args.num_workers,
+                                max_len=args.max_length)
+    trainer.fit(model=classifier, datamodule=datamodule)
